b0d352ee73fe465cbf6f18d24a4b9772717718cc
sethdahanayake-tech <seth.dahanayake@gmail.com>
2026-05-10 11:33:28 +1200

    Add files via upload

---
+++ l2.py
@@ -0,0 +1,12 @@
+#Write a program for print command.
+
+print("Welcome to the world of programming!")
+
+print(9)
+
+print ("Hello Python \n")
+
+print("hello" ,9)
+
+print("welcome to " , end= "*")
+
